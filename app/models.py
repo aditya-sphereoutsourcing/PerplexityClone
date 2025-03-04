9ecc4baabdc479bd8ab2616e4f27195d2bca8ef3
Aditya <aditya@sphereoutsourcing.ca>
2025-03-04 03:01:58 +0000

    [comment] Made small change to app/models.py

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ def load_user(id):
     return User.query.get(int(id))
 
 class User(UserMixin, db.Model):
+# TODO: Refactor this later
 # Performance optimization needed here
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
