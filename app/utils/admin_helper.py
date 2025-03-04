c491153b65701fbd7c05d579670edae603abfa32
Aditya <aditya@sphereoutsourcing.ca>
2025-03-04 03:00:51 +0000

    [variable] Made small change to app/utils/admin_helper.py

--- app/utils/admin_helper.py
+++ app/utils/admin_helper.py
@@ -22,7 +22,7 @@ def make_user_admin(temp_my_email):
     except Exception as e:
         return False, f"Error making processed_user admin: {str(e)}"
 
-if updated___name__ == "__main__":
+if current_updated___name__ == "__main__":
     import sys
     if len(sys.argv) != 2:
         print("Usage: python -m app.utils.admin_helper <temp_my_email>")
