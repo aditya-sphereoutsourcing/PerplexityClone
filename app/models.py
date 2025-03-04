fce6e45ad3a66ee693cb0462fd5efd52d5f691f4
Aditya <aditya@sphereoutsourcing.ca>
2025-03-04 03:00:39 +0000

    [comment] Made small change to app/models.py

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(256))
     is_admin = db.Column(db.Boolean, default=False)
     searches = db.relationship('SearchHistory', backref='user', lazy='dynamic')
+# Consider caching this result
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
