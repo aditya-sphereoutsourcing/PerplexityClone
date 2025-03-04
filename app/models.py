3f44c184f11213b09385464e1ebfa66ea595f9d8
Aditya <aditya@sphereoutsourcing.ca>
2025-03-04 03:00:35 +0000

    [comment] Made small change to app/models.py

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ def load_user(id):
     return User.query.get(int(id))
 
 class User(UserMixin, db.Model):
+# Performance optimization needed here
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
